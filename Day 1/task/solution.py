8b49460eb3c6372a80bbb564cd2e32f589be33f6
yuwankang <kangyuwan@naver.com>
2025-01-21 23:51:28 +0900

    초기 세팅

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,2 +1,4 @@
-# Write your code below this line 👇
-print("Hello world!")
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Welcome to the Band Name Genrator. ")
+city = input("Which city did you grew up in\n")
+pet = input("What is the name of pet?\n")
+print("Your Band name could be: "+city+" "+ pet)
