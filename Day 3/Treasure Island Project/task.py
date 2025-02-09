69538fc8ea7c34610a4c84fe1e9536e9e76249b0
yuwankang <kangyuwan@naver.com>
2025-02-09 19:33:03 +0900

    Day 3 클리어

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -22,3 +22,30 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a crossroad, where do you want to go? Type "left" or "right".').lower()
+
+if choice1 == "left":
+    choice2 = input('You\'ve come to lake. '
+                    'There is an island in the middle of the lake'
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.').lower()
+    if choice2 == "wait":
+        choice3 = input("you arrive at the island unharmed."
+                        "There is house with 3 doors. One red,"
+                        "one yellow and one blue."
+                        "Which color do you choose?").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over")
+        elif choice3 == "yellow":
+            print("You found the treasure. You win!")
+        elif choice3 == "blue":
+            print("You enter a room beasts. Game Over")
+        else:
+            print("You choose a door that dosen't exist. Game Over")
+
+    else:
+        print("You got attacked by an angry trout. Game over.")
+else:
+    print("You fell in to a hole. Game Over.")
+
+
